952532264e153897673bb439f88c926eb52a0f40
Kappaccinoh <lim.jia28@gmail.com>
2023-09-19 00:42:11 +0800

    lab 4

--- labs/lab4/inputArrayCount.asm
+++ labs/lab4/inputArrayCount.asm
@@ -1,25 +1,39 @@
 # arrayCount.asm
   .data 
-arrayA: .word 11, 0, 31, 22, 9, 17, 6, 9   # arrayA has 5 values
+arrayA: .word 0, 0, 0, 0, 0, 0, 0, 0   # arrayA has 5 values
 count:  .word 999             # dummy value
 userInput: .word 0
   .text
 main:
     # code to setup the variable mappings
-    la $t0, arrayA
-    la $t8, count
+    # la $t0, arrayA
+    # la $t8, count
+
+    # code for reading in the user value X
+    move $t0, $0
+    move $t1, $0
+    addi $t1, $t1, 32
+INPUTLOOP:
+    beq $t0, $t1, INPUTLOOPEND
+    li   $v0, 5
+    syscall # read user input
+    sw $v0, arrayA($t0)
+    addi $t0, $t0, 4
+    j INPUTLOOP
+INPUTLOOPEND:
+
 
     # code for reading in the user value X
     li   $v0, 5
     syscall # read user input
     sw $v0, userInput
-
     # code for counting multiples of X in arrayA
     lw $t8, userInput
     addi $t5, $t8, -1 # $t5 is your mask
 
     move $t6, $0
     move $t0, $0
+    move $t1, $0
     addi $t1, $t1, 32
 
 LOOP: beq $t0, $t1, END
