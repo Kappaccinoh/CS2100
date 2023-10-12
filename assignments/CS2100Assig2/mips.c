54059edc74d7cdba0e6dcd4dc87880cf5ee11910
Kappaccinoh <lim.jia28@gmail.com>
2023-10-12 20:50:55 +0800

    question 3

--- assignments/CS2100Assig2/mips.c
+++ assignments/CS2100Assig2/mips.c
@@ -119,7 +119,31 @@ void decode(uint32_t in, struct instr* insn) {
 void Control(uint8_t opcode, bool* _RegDst, bool* _ALUSrc, bool* _MemtoReg,
              bool* _RegWrite, bool* _MemRead, bool* _MemWrite, bool* _Branch,
              uint8_t* _ALUOp) {
-    // TODO: Implement Control
+    if (opcode == 0) *_RegDst = 1;
+    else *_RegDst = 0;
+
+    if (opcode == 0 || opcode == 4) *_ALUSrc = 0;
+    else *_ALUSrc = 1;
+
+    if (opcode == 35) *_MemRead = 1;
+    else *_MemRead = 0;
+
+    switch (opcode) {
+        case 0: *_ALUOp = 2;
+            break;
+        case 35:
+        case 43:
+            *_ALUOp = 0;
+            break;
+        case 4:
+            *_ALUOp = 1;
+            break;
+        default:
+    }
+
+    if (opcode == 2 || opcode == 3) {
+        *_Branch = 1;
+    }
 }
 
 #endif  // End of Assignment 2, Question 2a
@@ -127,8 +151,26 @@ void Control(uint8_t opcode, bool* _RegDst, bool* _ALUSrc, bool* _MemtoReg,
 #ifndef ASSIGNMENT2_QUESTION2B
 
 uint8_t ALUControl(uint8_t ALUOp, uint8_t funct) {
-    // TODO: Implement ALUControl
-    return -1;
+    if (ALUOp == 2) { // R Format Instructions
+        switch (funct) {
+            case 32:
+                return 2;
+            case 34:
+                return 6;
+            case 36:
+                return 0;
+            case 39:
+                return 12;
+            case 37:
+                return 1;
+            case 42:
+                return 7;
+        }
+    } else if (ALUOp == 1) { // BEQ Instruction
+        return 6;
+    } else { // lw/sw
+        return 2;
+    }
 }
 
 #endif  // End of Assignment 2, Question 2b
@@ -136,8 +178,20 @@ uint8_t ALUControl(uint8_t ALUOp, uint8_t funct) {
 #ifndef ASSIGNMENT2_QUESTION2C
 
 int32_t ALU(int32_t in0, int32_t in1, uint8_t ALUControl, bool* ALUiszero) {
-    // TODO: Implement ALU
-    return -1;
+    int32_t result;
+
+    switch (ALUControl) {
+        case 0: result = in0 & in1;
+        case 1: result = in0 | in1;
+        case 2: result = in0 + in1;
+        case 6: result = in0 - in1;
+        case 7: result = (in0 < in1) ? 1 : 0;
+        case 12: result = !(in0 | in1);
+    }
+
+    *ALUiszero = (result == 0) ? 1 : 0;
+
+    return result;
 }
 
 #endif  // End of Assignment 2, Question 2c
@@ -145,7 +199,51 @@ int32_t ALU(int32_t in0, int32_t in1, uint8_t ALUControl, bool* ALUiszero) {
 #ifndef ASSIGNMENT2_QUESTION3
 
 void execute(uint32_t insn) {
-    // TODO: Implement execute
+
+    // Stage 0 Update PC
+    _PC += 4;
+
+    // Stage 1 Decode - Convert Instruction and Populate 'instr' struct
+    struct instr instr;
+    decode(insn, &instr);
+
+
+    // Stage 2 Execute
+    // Set all the MUX control gates to their appropriate value based on the decoded opcode 
+    Control(instr.opcode, _RegDst, _ALUSrc, _MemtoReg, _RegWrite, _MemRead, _MemWrite, _Branch, _ALUOp);
+    // Set the ALU Control Signal to the appropriate signal based on the ALUOp and the Instruction Function
+    _ALUCtrl = ALUControl(_ALUOp, instr.funct);
+
+    // Stage 2.5 Branch
+    if ((instr.opcode == 2 || instr.opcode == 3) && _Branch == 1) {
+        int32_t temp = instr.address << 2;
+        int32_t u = (_PC >> (32 - 4)) << 32 - 4;
+        temp = temp | u;
+        _PC += temp;
+    }
+
+    // Setting the correct outputs of the file register
+    int32_t* RD1;
+    int32_t* RD2;
+    uint8_t WR = mux_u8(_RegDst, instr.rt, instr.rd);
+    int32_t WD;
+
+    // Read Register - Note that WD is not written yet, we will write it later
+    // once the value of WD has been determined - still impt because this step
+    // writes the values of RD1 and RD2, which are inputs for the ALU
+    RegFile(instr.rs, instr.rd, WR, WD, RD1, RD2, _RegWrite);
+
+    // Configuring ALU with the correct signals
+    bool* _ALUisZero;
+    int32_t ALUResult = ALU(RD1, RD2, _ALUCtrl, _ALUisZero);
+
+
+    // Stage 3 Memory
+    int32_t MemOutput = Memory(ALUResult, RD2, _MemRead, _MemWrite);
+
+
+    // Stage 4 WriteBack
+    RegFile(instr.rs, instr.rd, WR, MemOutput, RD1, RD2, _RegWrite);
 }
 
 #endif  // End of Assignment 2, Question 3
